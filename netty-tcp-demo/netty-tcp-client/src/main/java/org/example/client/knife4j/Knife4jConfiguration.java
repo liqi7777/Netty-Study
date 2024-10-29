67c0630c5fbc8fe1bf0e525c9c7dc6b2bc6a85ff
liqi <963310476@qq.com>
2024-10-29 15:59:02 +0800

    增加Knife4j，接口文档！

---
+++ netty-tcp-demo/netty-tcp-client/src/main/java/org/example/client/knife4j/Knife4jConfiguration.java
@@ -0,0 +1,48 @@
+package org.example.client.knife4j;
+
+
+/*
+ * Copyright (C) 2018 Zhejiang xiaominfo Technology CO.,LTD.
+ * All rights reserved.
+ * Official Web Site: http://www.xiaominfo.com.
+ * Developer Web Site: http://open.xiaominfo.com.
+ */
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @author liqi
+ * create  2024/10/29 3:20 下午
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("http://www.xx.com/")
+                        .contact("xx@qq.com")
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("2.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("org.example.client.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+}
